03a9377ba313cfb15e9fb83b23bdd2f73bf1824c
zntchantchou <z.ntchantchou@gmail.com>
2024-10-18 09:21:14 +0200

    sidebar and navbar ok

--- client/src/app/(components)/Navbar/index.tsx
+++ client/src/app/(components)/Navbar/index.tsx
@@ -1,24 +1,36 @@
 "use client";
-import { Bell, Menu, Settings, Sun } from "lucide-react";
+import { useAppDispatch, useAppSelector } from "@/app/redux";
+import { setIsDarkMode, setIsSidebarCollapsed } from "@/state";
+import { Bell, Menu, Moon, Settings, Sun } from "lucide-react";
 import Link from "next/link";
 
 function Navbar() {
+  const dispatch = useAppDispatch();
+  const isSidebarCollapsed = useAppSelector(
+    (state) => state.global.isSidebarCollapsed
+  );
+  const toggleSidebar = () => {
+    dispatch(setIsSidebarCollapsed(!isSidebarCollapsed));
+  };
+  const isDarkMode = useAppSelector((state) => state.global.isDarkMode);
+  const toggleDarkMode = () => {
+    dispatch(setIsDarkMode(!isDarkMode));
+  };
+
   return (
     <div className={`flex justify-between items-center w-full mb-7`}>
       {/* LEFT SIDE */}
       <div className="flex justify-between items-center gap-5">
         <button
           className="px-3 py-3 bg-gray-100 rounded-full hover:bg-blue-100"
-          // onClick={() => {
-          //   console.log("Clicked btn");
-          // }}
+          onClick={toggleSidebar}
         >
           <Menu className="w-4 h-4" />
         </button>
 
         <div className="relative">
           <input
-            className="pl-10 pr-4 py-2 w-50 md:w-80 border-2 border-gray-300 bg-white rounded-lg focus:outline-none focus:border-blue-500"
+            className="pl-10 pr-4 py-2 w-50 md:w-60 border-2 border-gray-300 bg-white rounded-lg focus:outline-none focus:border-blue-500"
             type="search"
             placeholder="type to search groups & products"
           />
@@ -31,8 +43,12 @@ function Navbar() {
       <div className="flex justify-between items-center gap-5">
         <div className="hidden md:flex justify-between items-center gap-5">
           <div>
-            <button onClick={() => {}}>
-              <Sun className="cursor-pointer text-gray-500" size={24} />
+            <button onClick={toggleDarkMode}>
+              {isDarkMode ? (
+                <Sun className="cursor-pointer text-gray-500" size={24} />
+              ) : (
+                <Moon className="cursor-pointer text-gray-500" size={24} />
+              )}
             </button>
           </div>
           <div className="relative">
--- client/src/app/dashboard/DashboardWrapper.tsx
+++ client/src/app/dashboard/DashboardWrapper.tsx
@@ -1,15 +1,46 @@
-import Navbar from "@/app/(components)/Navbar";
-import Sidebar from "@/app/(components)/Navbar/Sidebar";
+"use client";
 
-function DashboardWrapper({ children }: { children: React.ReactNode }) {
+import Navbar from "@/app/(components)/Navbar";
+import Sidebar from "@/app/(components)/Sidebar";
+import StoreProvider, { useAppSelector } from "../redux";
+import { useEffect } from "react";
+
+function DashboardLayout({ children }: { children: React.ReactNode }) {
+  const isSidebarCollapsed = useAppSelector(
+    (state) => state.global.isSidebarCollapsed
+  );
+  const isDarkMode = useAppSelector((state) => state.global.isDarkMode);
+  const mainPaddingLeft = isSidebarCollapsed ? "md:pl-24" : "md:pl-72";
+
+  useEffect(() => {
+    if (isDarkMode) {
+      document.documentElement.classList.add("dark");
+    } else {
+      document.documentElement.classList.add("light");
+    }
+  });
   return (
-    <div className={`light flex bg-gray-50 text-gray-900 w-full min-h-screen`}>
+    <div
+      className={`${
+        isDarkMode ? "dark" : "light"
+      } flex bg-gray-50 text-gray-900 w-full min-h-screen`}
+    >
       <Sidebar></Sidebar>
-      <main className="flex flex-col w-full py-7 px-9 bg-gray-100 md:pl-72">
+      <main
+        className={`flex flex-col w-full py-7 px-9 bg-gray-100 ${mainPaddingLeft}`}
+      >
         <Navbar></Navbar>
         {children}
       </main>
     </div>
+  );
+}
+
+function DashboardWrapper({ children }: { children: React.ReactNode }) {
+  return (
+    <StoreProvider>
+      <DashboardLayout>{children}</DashboardLayout>
+    </StoreProvider>
   );
 }
 
